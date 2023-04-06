6c419a99d8ce98b4d1c0c4be8547495ecfdfb14f
FawadHussainShah512 <107517943+FawadHussainShah512@users.noreply.github.com>
2023-04-06 13:18:28 +0500

    Create simple_interest.sh

---
+++ simple_interest.sh
@@ -0,0 +1,38 @@
+# Contributing to Simple Interest Script
+
+Thank you for your interest in contributing to the Simple Interest Script! We welcome contributions from the community to improve our script and make it better for everyone. By contributing to this project, you agree to abide by the following guidelines:
+
+## Code of Conduct
+
+Please review and adhere to our [Code of Conduct](CODE_OF_CONDUCT.md) at all times when participating in our project. We strive to maintain a respectful and inclusive community where everyone feels welcome and valued.
+
+## Getting Started
+
+To contribute to the Simple Interest Script, follow these steps:
+
+1. Fork the repository to your GitHub account.
+2. Clone the forked repository to your local machine.
+3. Create a new branch for your contribution, with a descriptive name that reflects the purpose of your changes.
+4. Make your changes and test them thoroughly.
+5. Commit your changes with clear and descriptive commit messages.
+6. Push your changes to your forked repository.
+7. Submit a pull request to the main repository, detailing the changes you made and the reason for the contribution.
+
+## Contribution Guidelines
+
+- Follow the bash scripting best practices and style guidelines.
+- Provide clear and concise code comments and documentation for your changes.
+- Test your changes thoroughly and ensure they do not introduce any regressions.
+- Be respectful and professional in your interactions with other contributors and maintainers.
+- Respond to feedback and comments promptly, and be open to constructive criticism.
+- If your contribution requires a significant change or addition, please discuss it with the maintainers beforehand to ensure it aligns with the project's goals and direction.
+
+## License
+
+By contributing to the Simple Interest Script, you agree to license your contributions under the [MIT License](LICENSE), unless otherwise specified.
+
+## Acknowledgments
+
+We greatly appreciate your contributions to the Simple Interest Script and would like to acknowledge and thank all the contributors who have helped make this project better.
+
+Happy contributing!
